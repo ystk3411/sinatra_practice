132332317678822105e79d9b7391e095aa2758d8
ystk3411 <yoshi.hama0701@gmail.com>
2026-07-16 00:10:05 +0900

    IDの採番をuuidを使った方式に修正

--- myapp.rb
+++ myapp.rb
@@ -3,6 +3,7 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'json'
+require 'securerandom'
 use Rack::MethodOverride
 JSON_FILE = 'memo_data.json'
 
@@ -17,40 +18,39 @@ end
 
 post '/memo' do
   memo_data = parse_json
-  new_id = memo_data[:data].empty? ? 1 : memo_data[:data][-1][:id] + 1
-  new_memo = { id: new_id, title: params[:title], content: params[:content] }
+  new_memo = { id: SecureRandom.uuid, title: params[:title], content: params[:content] }
   memo_data[:data] << new_memo
   File.open(JSON_FILE, 'w') do |file|
-    file.write(JSON.pretty_generate(memo_data))
+    file.write(JSON.generate(memo_data))
   end
   redirect '/'
 end
 
 get '/memo/:id' do
-  @memo_data = parse_json[:data].find { |memo| memo[:id] == params[:id].to_i }
+  @memo_data = parse_json[:data].find { |memo| memo[:id] == params[:id] }
   erb :show
 end
 
 get '/memo/:id/edit' do
-  @memo_data = parse_json[:data].find { |memo| memo[:id] == params[:id].to_i }
+  @memo_data = parse_json[:data].find { |memo| memo[:id] == params[:id] }
   erb :edit
 end
 
 patch '/memo/:id' do
   memo_data = parse_json
-  update_memo_data = memo_data[:data].find { |memo| memo[:id] == params[:id].to_i }
+  update_memo_data = memo_data[:data].find { |memo| memo[:id] == params[:id] }
   update_memo_data[:title] = params[:title]
   update_memo_data[:content] = params[:content]
 
   File.open(JSON_FILE, 'w') do |file|
     file.write(JSON.pretty_generate(memo_data))
   end
-  redirect "/memo/#{params[:id].to_i}"
+  redirect "/memo/#{params[:id]}"
 end
 
 delete '/memo/:id' do
   memo_data = parse_json
-  memo_data[:data].delete_if { |memo| memo[:id] == params[:id].to_i }
+  memo_data[:data].delete_if { |memo| memo[:id] == params[:id] }
   File.open(JSON_FILE, 'w') do |file|
     file.write(JSON.pretty_generate(memo_data))
   end
