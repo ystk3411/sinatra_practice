bb3bc000135c449231eb383e6be03605d7f05451
ystk3411 <yoshi.hama0701@gmail.com>
2026-07-15 20:54:14 +0900

    ファイル名を定数化

--- myapp.rb
+++ myapp.rb
@@ -4,6 +4,7 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'json'
 use Rack::MethodOverride
+JSON_FILE = 'memo_data.json'
 
 get '/' do
   @memo_data = parse_json
@@ -16,11 +17,10 @@ end
 
 post '/memo' do
   memo_data = parse_json
-  p memo_data[:data]
   new_id = memo_data[:data].empty? ? 1 : memo_data[:data][-1][:id] + 1
   new_memo = { id: new_id, title: params[:title], content: params[:content] }
   memo_data[:data] << new_memo
-  File.open('MemoData.json', 'w') do |file|
+  File.open(JSON_FILE, 'w') do |file|
     file.write(JSON.pretty_generate(memo_data))
   end
   redirect '/'
@@ -38,13 +38,11 @@ end
 
 patch '/memo/:id' do
   memo_data = parse_json
-  # memo_data[:data][params[:id].to_i - 1][:title] = params[:title]
-  # memo_data[:data][params[:id].to_i - 1][:content] = params[:content]
   update_memo_data = memo_data[:data].find { |memo| memo[:id] == params[:id].to_i }
   update_memo_data[:title] = params[:title]
   update_memo_data[:content] = params[:content]
 
-  File.open('MemoData.json', 'w') do |file|
+  File.open(JSON_FILE, 'w') do |file|
     file.write(JSON.pretty_generate(memo_data))
   end
   redirect "/memo/#{params[:id].to_i}"
@@ -53,14 +51,14 @@ end
 delete '/memo/:id' do
   memo_data = parse_json
   memo_data[:data].delete_if { |memo| memo[:id] == params[:id].to_i }
-  File.open('MemoData.json', 'w') do |file|
+  File.open(JSON_FILE, 'w') do |file|
     file.write(JSON.pretty_generate(memo_data))
   end
   redirect '/'
 end
 
 def parse_json
-  JSON.parse(File.read('MemoData.json'), symbolize_names: true)
+  JSON.parse(File.read(JSON_FILE), symbolize_names: true)
 end
 
 helpers do
